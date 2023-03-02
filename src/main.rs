54d069198a0f4bb779a7457607a1fbdb54f7ef55
ccl <ccl@ccls-Laptop.local>
2023-03-03 00:41:32 +0800

    Support major triad generation given root

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
 use std::env;
 use std::process;
 use std::str::FromStr;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Eq, Hash, PartialEq)]
 enum Note {
     C,
     Db,
@@ -40,6 +41,29 @@ impl FromStr for Note {
     }
 }
 
+pub trait ToInt {
+    fn to_int(&self) -> i32;
+}
+
+impl ToInt for Note {
+    fn to_int(&self) -> i32 {
+        match self {
+            Note::C => 0,
+            Note::Db => 1,
+            Note::D => 2,
+            Note::Eb => 3,
+            Note::E => 4,
+            Note::F => 5,
+            Note::Gb => 6,
+            Note::G => 7,
+            Note::Ab => 8,
+            Note::A => 9,
+            Note::Bb => 10,
+            Note::B => 11,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Config {
     root: Note,
@@ -56,10 +80,28 @@ impl Config {
 }
 
 fn main() {
+    let notes_int = 0..12;
+    let notes_str = [
+        "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B",
+    ];
+    let notes = notes_str.iter().map(|e| Note::from_str(e).unwrap());
+    let note2int: HashMap<i32, Note> = HashMap::from_iter(notes_int.zip(notes));
+
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Wrong input: {err}");
         process::exit(1);
     });
     println!("the root note is {:?}", config.root);
+    let major_triad = generate_major_triad(config.root, &note2int);
+    println!("the major triad is {:#?}", major_triad);
+}
+
+fn generate_major_triad(root: Note, note_map: &HashMap<i32, Note>) -> [&Note; 3] {
+    let root_int = root.to_int();
+    let triad_notes_int = [root_int, (root_int + 4) % 12, (root_int + 7) % 12];
+    println!("{:#?}", triad_notes_int);
+    let triad_notes: [&Note; 3] =
+        triad_notes_int.map(|e| note_map.get(&e).expect("value cannot be converted to note"));
+    triad_notes
 }
